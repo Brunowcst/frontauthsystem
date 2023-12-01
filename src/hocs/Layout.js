4964ee3e916d2d13a78630ad4a5a29a51df4694f
Brunowcst <brunoporfirio022@gmail.com>
2023-12-01 16:33:49 -0300

    Layout

---
+++ src/hocs/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/Navbar'
+
+const Layout = (props) => {    
+    return (
+        <div>
+            <NavBar/>
+            {props.children}
+        </div>
+    )
+}
+
+export default Layout;
+
