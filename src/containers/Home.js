0123a209dbf13f9d3b48dedb69a7bb3a080b59e7
Brunowcst <brunoporfirio022@gmail.com>
2023-12-02 19:03:57 -0300

    Atualiza home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div class="container col-6">
+      <div class="jumbotron">
+        <h1 class="display-4">Welcome to Auth System!</h1>
+        <hr class="my-4"/>
+        <p>Log in to your account to continue browsing</p>
+        <p class="lead">
+          <Link class="btn btn-primary btn-lg" to='/login' role="button">Login</Link>
+        </p>
+      </div>
+    </div>
   )
 }
