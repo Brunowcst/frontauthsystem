d4a833487b9d67c957011761ff6fcea9afdf8c87
Brunowcst <brunoporfirio022@gmail.com>
2023-12-02 19:06:55 -0300

    Layout

--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../components/Navbar'
+import { connect } from 'react-redux'
+import { check_authenticated, load_user } from '../actions/auth'
 
-const Layout = (props) => {    
+const Layout = (props) => {   
+    useEffect(() => {
+        props.check_authenticated();
+        props.load_user()
+    }, [])
+    
     return (
         <div>
             <NavBar/>
@@ -10,5 +17,5 @@ const Layout = (props) => {
     )
 }
 
-export default Layout;
+export default connect(null, {check_authenticated, load_user})(Layout)
 
