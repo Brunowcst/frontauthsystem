67dcd960020706a0fb71cd1808d8bfb91048b668
Brunowcst <brunoporfirio022@gmail.com>
2023-12-02 19:04:14 -0300

    Adiciona funções na página de login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const Login = () => {
+import { login } from '../actions/auth';
+
+const Login = ({ login, isAuthenticated }) => {
+
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -14,9 +19,53 @@ const Login = () => {
     setFormData({...formData, [e.target.name]: e.target.value})
   }
 
+  const onSubmit = e => {
+    e.preventDefault()
+    login(email, password)
+  }
+
+  if (isAuthenticated) {
+    return navigate('/')
+  } 
+
   return (
-    <p></p>
+    <div className='container flex-column mt-5 d-flex align-items-center text-center'>
+      <h1>Login</h1>
+      <p>Entre em sua conta</p>
+      <form onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            className='form-control mb-3' 
+            type="text"
+            placeholder='Email'
+            name='email'
+            value={email} 
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group mb-3 '>
+          <input
+            className='form-control' 
+            type="password"
+            placeholder='Password'
+            name='password'
+            value={password} 
+            onChange={e => onChange(e)}
+            required
+            minLength={8}
+          />
+        </div>
+        <button type='submit' className='btn btn-primary mx-auto d-flex'>Entrar</button>
+      </form>
+      <p className='mt-3'>Don't have an account <Link to="/signup">Sign Up</Link></p>
+      <p className='mt-3'>Forget your password? <Link to="/reset-password">Reset password</Link></p>
+    </div>
   );
 };
 
-export default Login;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { login })(Login);
