3f55e67d5b8154d8e063e16f11a5b769c06cbd01
Brunowcst <brunoporfirio022@gmail.com>
2023-12-02 19:07:05 -0300

    Adicona provider

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,19 +9,24 @@ import SignUp from "../containers/SignUp";
 
 import Layout from "../hocs/Layout";
 
+import { Provider } from "react-redux";
+import store from "../store";
+
 export function RoutesApp() {
     return (
-        <Router>
-            <Layout>
-                <Routes>
-                    <Route path="/" Component={Home}/>
-                    <Route path="/login" Component={Login}/>
-                    <Route path="/signup" Component={SignUp}/>
-                    <Route path="/activate/:uid/:token" Component={Activate}/>
-                    <Route path="/reset_password" Component={ResetPassword}/>
-                    <Route path="/password/reset/confirm/:uid/:token" Component={ResetPasswordConfirm}/>
-                </Routes>
-            </Layout>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Layout>
+                    <Routes>
+                        <Route path="/" Component={Home}/>
+                        <Route path="/login" Component={Login}/>
+                        <Route path="/signup" Component={SignUp}/>
+                        <Route path="/activate/:uid/:token" Component={Activate}/>
+                        <Route path="/reset-password" Component={ResetPassword}/>
+                        <Route path="/password/reset/confirm/:uid/:token" Component={ResetPasswordConfirm}/>
+                    </Routes>
+                </Layout>
+            </Router>
+        </Provider>
     )
 }
