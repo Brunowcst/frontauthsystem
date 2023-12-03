e6e0f21b17a7dd4660987692fbc07e8aabb27345
Brunowcst <brunoporfirio022@gmail.com>
2023-12-03 15:41:09 -0300

    Atualiza página de ativação

--- src/containers/Activate.js
+++ src/containers/Activate.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function Activate() {
+import { user_activation } from '../actions/auth';
+
+const ActivateUser = ({ user_activation }) => {
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const [verify, setVerify] = useState(false)
+  
+  const onSubmit = e => {
+    e.preventDefault()
+
+    const uid = params.uid
+    const token = params.token
+
+    user_activation(uid, token)
+    setVerify(true)
+  }
+
+  if (verify) {
+    return navigate('/')
+  } 
+
   return (
-    <div>Activate</div>
-  )
-}
+    <div className='container flex-column mt-5 d-flex align-items-center text-center'>
+      <h1>Activation</h1>
+      <p>Active your account now</p>
+      <form onSubmit={e => onSubmit(e)}>
+        <button type='submit' className='btn btn-primary mx-auto d-flex'>Activate</button>
+      </form>
+      <p className='mt-3'>You will receive an email with confirmation</p>
+    </div>
+  );
+};
+
+export default connect(null, { user_activation })(ActivateUser);
