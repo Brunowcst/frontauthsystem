a251fe9524cfa1c971c335ae30c0ac5b91d693ad
Brunowcst <brunoporfirio022@gmail.com>
2023-12-03 01:05:35 -0300

    atualiza página de reset password confirm

--- src/containers/ResetPasswordConfirm.js
+++ src/containers/ResetPasswordConfirm.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function ResetPasswordConfirm() {
+import { reset_password_confirm } from '../actions/auth';
+
+const ResetPasswordConfirm = ({ reset_password_confirm }) => {
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const [requestSend, setRequestSend] = useState(false)
+  const [formData, setFormData] = useState({
+    newpassword: '',
+    renewpassword: ''
+  })
+
+  const {newpassword, renewpassword} = formData
+
+  const onChange = e => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+    const uid = params.uid
+    const token = params.token
+
+    if(newpassword === renewpassword) {
+      setRequestSend(true)
+      reset_password_confirm(uid, token, newpassword, renewpassword)
+    } else {
+      console.log('error linha 27')
+    }
+  }
+
+  if (requestSend) {
+    return navigate('/')
+  } 
+
   return (
-    <div>ResetPasswordConfirm</div>
-  )
-}
+    <div className='container flex-column mt-5 d-flex align-items-center text-center'>
+      <h1>Reset Password</h1>
+      <p>Request password reset</p>
+      <form onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            className='form-control mb-3' 
+            type="password"
+            placeholder='New password'
+            name='newpassword'
+            value={newpassword} 
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            className='form-control mb-3' 
+            type="password"
+            placeholder='Confirm the Password'
+            name='renewpassword'
+            value={renewpassword} 
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <button type='submit' className='btn btn-primary mx-auto d-flex'>Reset password</button>
+      </form>
+      <p className='mt-3'>You will receive an email with confirmation</p>
+    </div>
+  );
+};
+
+export default connect(null, { reset_password_confirm })(ResetPasswordConfirm);
